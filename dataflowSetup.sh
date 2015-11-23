46065e311abe248ce4f2ff0f7ed8a5a45c675417
Jason <jianchuli0624@gmail.com>
2015-11-23 13:30:33 -0500

    gradle successfully

--- dataflowSetup.sh
+++ dataflowSetup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+export MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-export MYDIR=`dirname $0`
 cp -r $MYDIR/src/dataflow $MYDIR/../checker-framework-inference/src
+mv $MYDIR/../checker-framework-inference/src/dataflow/tests/DataflowTest.java $MYDIR/../checker-framework-inference/tests/checkers/inference
+cp -r $MYDIR/../checker-framework-inference/src/dataflow/tests/testdata/dataflow $MYDIR/../checker-framework-inference/testdata
 cp -r $MYDIR/src/dataflow/DataflowExample $MYDIR/..
-gradle -b ../checker-framework-inference/build.gradle dist
+gradle -b $MYDIR/../checker-framework-inference/build.gradle dist
