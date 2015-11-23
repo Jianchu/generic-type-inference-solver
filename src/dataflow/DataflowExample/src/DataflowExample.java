17bb945d170b736d2f962fe4912dc4f0b4fcda6a
Jason <jianchuli0624@gmail.com>
2015-11-22 21:13:24 -0500

    add DataflowExample

---
+++ src/dataflow/DataflowExample/src/DataflowExample.java
@@ -0,0 +1,7 @@
+package AntExample;
+import dataflow.quals.DataFlow;
+
+public class DataflowExample {
+	@dataflow.quals.DataFlow(typeNames={"java.lang.String"}) String annotatedString = "I am a String!";
+	String insertAnnotationToThis = annotatedString;
+}
