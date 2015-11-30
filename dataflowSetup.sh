8b44761ce538b1b0817e57e61bc93f88570ad0d1
Jianchu <jianchuli0624@gmail.com>
2015-11-30 11:26:24 -0500

    change cp to ln

--- dataflowSetup.sh
+++ dataflowSetup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cp -r $MYDIR/src/dataflow $MYDIR/../checker-framework-inference/src
+ln -s $MYDIR/src/dataflow $MYDIR/../checker-framework-inference/src
 mv $MYDIR/../checker-framework-inference/src/dataflow/tests/DataflowTest.java $MYDIR/../checker-framework-inference/tests/checkers/inference
-cp -r $MYDIR/../checker-framework-inference/src/dataflow/tests/testdata/dataflow $MYDIR/../checker-framework-inference/testdata
-cp -r $MYDIR/src/dataflow/DataflowExample $MYDIR/..
+ln -s $MYDIR/../checker-framework-inference/src/dataflow/tests/testdata/dataflow $MYDIR/../checker-framework-inference/testdata
+ln -s $MYDIR/src/dataflow/DataflowExample $MYDIR/..
 gradle -b $MYDIR/../checker-framework-inference/build.gradle dist
