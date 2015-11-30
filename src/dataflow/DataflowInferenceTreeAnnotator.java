458c5a2db4448535ac3afe5a2f21257578e83067
Jason <jianchuli0624@gmail.com>
2015-11-30 18:58:30 -0500

    add DataflowInferenceTreeAnnotator

---
+++ src/dataflow/DataflowInferenceTreeAnnotator.java
@@ -0,0 +1,74 @@
+package dataflow;
+
+import org.checkerframework.framework.type.AnnotatedTypeFactory;
+import org.checkerframework.framework.type.AnnotatedTypeMirror;
+
+import javax.lang.model.element.AnnotationMirror;
+
+import checkers.inference.InferenceAnnotatedTypeFactory;
+import checkers.inference.InferenceTreeAnnotator;
+import checkers.inference.InferrableChecker;
+import checkers.inference.SlotManager;
+import checkers.inference.VariableAnnotator;
+import checkers.inference.model.ConstantSlot;
+
+import com.sun.source.tree.LiteralTree;
+import com.sun.source.tree.NewClassTree;
+
+import dataflow.util.DataflowUtils;
+
+public class DataflowInferenceTreeAnnotator extends InferenceTreeAnnotator {
+
+    private final SlotManager slotManager;
+    private final VariableAnnotator variableAnnotator;
+    private final AnnotatedTypeFactory realTypeFactory;
+    private final InferrableChecker realChecker;
+
+    public DataflowInferenceTreeAnnotator(
+            InferenceAnnotatedTypeFactory atypeFactory,
+            InferrableChecker realChecker,
+            AnnotatedTypeFactory realAnnotatedTypeFactory,
+            VariableAnnotator variableAnnotator, SlotManager slotManager) {
+        super(atypeFactory, realChecker, realAnnotatedTypeFactory,
+                variableAnnotator, slotManager);
+
+        this.slotManager = slotManager;
+        this.variableAnnotator = variableAnnotator;
+        this.realTypeFactory = realAnnotatedTypeFactory;
+        this.realChecker = realChecker;
+    }
+
+    @Override
+    public Void visitLiteral(final LiteralTree literalTree,
+            final AnnotatedTypeMirror atm) {
+        AnnotationMirror anno = DataflowUtils.generateDataflowAnnoFromLiteral(
+                literalTree, atm, this.realTypeFactory.getProcessingEnv());
+        variableAnnotator.createConstant(anno, literalTree);
+        variableAnnotator.visit(atm, literalTree);
+        return null;
+    }
+
+    @Override
+    public Void visitNewClass(final NewClassTree newClassTree,
+            final AnnotatedTypeMirror atm) {
+        System.out.println("visitNewClass is called!");
+        System.out.println("ATM: " + atm.toString());
+        AnnotationMirror anno = DataflowUtils
+                .genereateDataflowAnnoFromNewClass(newClassTree, atm,
+                this.realTypeFactory.getProcessingEnv());
+        ConstantSlot cs = variableAnnotator.createConstant(anno, newClassTree);
+        atm.addAnnotation(cs.getValue());
+
+        // DataflowAnnotatedTypeFactory realTypeFacotry =
+        // (DataflowAnnotatedTypeFactory) this.realChecker
+        // .createRealTypeFactory();
+        // DataflowTreeAnnotator realTreeAnnotator = (DataflowTreeAnnotator)
+        // realTypeFacotry
+        // .getDataflowTreeAnnotator();
+        // realTreeAnnotator.visitNewClass(newClassTree, atm);
+        variableAnnotator.visit(atm, newClassTree.getIdentifier());
+        System.out.println(anno.toString());
+        return null;
+    }
+
+}
