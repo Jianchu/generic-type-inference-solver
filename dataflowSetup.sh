1a46c134256f397c2def2e042adbe6161b282ae4
Jason <jianchuli0624@gmail.com>
2015-11-30 19:51:08 -0500

    change setup script

--- dataflowSetup.sh
+++ dataflowSetup.sh
@@ -4,5 +4,5 @@ export MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -s $MYDIR/src/dataflow $MYDIR/../checker-framework-inference/src
 mv $MYDIR/../checker-framework-inference/src/dataflow/tests/DataflowTest.java $MYDIR/../checker-framework-inference/tests/checkers/inference
 ln -s $MYDIR/../checker-framework-inference/src/dataflow/tests/testdata/dataflow $MYDIR/../checker-framework-inference/testdata
-ln -s $MYDIR/src/dataflow/DataflowExample $MYDIR/..
+ln -s $MYDIR/src/dataflow/dataflowexample $MYDIR/..
 gradle -b $MYDIR/../checker-framework-inference/build.gradle dist
