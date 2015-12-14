0b7c205398f6609824cd538e851442bf4682aeef
Jason <jianchuli0624@gmail.com>
2015-12-13 21:01:44 -0500

    add CFI error demo

---
+++ demo/libs/src/Foo.java
@@ -0,0 +1,13 @@
+package libs;
+
+public class Foo {
+	private int fieldOfFoo;
+
+	public Foo(int fieldOfFoo){
+		this.fieldOfFoo = fieldOfFoo;
+	}
+
+	public int getField(){
+		return fieldOfFoo;
+	}
+}
---
+++ demo/project/src/Bar.java
@@ -0,0 +1,13 @@
+package project;
+
+import libs.Foo;
+
+public class Bar {
+
+	private Foo fieldOfBar;
+
+	public Bar(){
+		this.fieldOfBar = new Foo(10);
+	}
+
+}
