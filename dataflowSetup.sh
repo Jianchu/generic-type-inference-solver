4920b340ab1db0a7eaac91dd1e1a3f7a5eedeb0e
Jason <jianchuli0624@gmail.com>
2015-12-21 13:08:25 -0500

    and ant compile-libs

--- dataflowSetup.sh
+++ dataflowSetup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+ant -buildfile $MYDIR/src/dataflow/dataflowexample/build.xml compile-libs
 ln -s $MYDIR/src/dataflow $MYDIR/../checker-framework-inference/src
 mv $MYDIR/../checker-framework-inference/src/dataflow/tests/DataflowTest.java $MYDIR/../checker-framework-inference/tests/checkers/inference
 ln -s $MYDIR/../checker-framework-inference/src/dataflow/tests/testdata/dataflow $MYDIR/../checker-framework-inference/testdata
