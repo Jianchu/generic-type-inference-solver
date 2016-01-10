bb47bef6a370f7c7fd62ee39f705b54cef53be01
Jason <jianchuli0624@gmail.com>
2016-01-10 16:03:14 -0500

    delete useless scripts

--- scripts/compileLogicSolver.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export MYDIR=`dirname $0`
-. $MYDIR/setup.sh
-mkdir $MYDIR/../bin/
-$JAVAC -source 7 -target 7 -d $MYDIR/../bin/ `find $MYDIR/../src/ -name "*.java"`
-
--- scripts/logicql_infer_ostrusted.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export MYDIR=`dirname $0`
-. $MYDIR/setup.sh
-
-java -ea -Xms512m -Xmx2048m -Xbootclasspath/p:$JSR308/checker-framework-inference/dist/json-simple-1.1.1.jar:$JSR308/checker-framework-inference/dist/mockito-all-1.8.4.jar:$CLASSPATH -ea -ea:checkers.inference... checkers.inference.InferenceCli --checker ostrusted.OsTrustedChecker --solver LogicSolver.LogicSolver  --log-level INFO --bootclasspath $JSR308/checker-framework-inference/dist/jdk7.jar $*
