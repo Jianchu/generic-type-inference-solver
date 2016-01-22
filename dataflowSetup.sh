d234f437fa5846db4c28e06baf786ed11ec964b2
Jianchu <jianchuli0624@gmail.com>
2016-01-22 18:39:56 -0500

    update setup

--- dataflowSetup.sh
+++ dataflowSetup.sh
@@ -3,7 +3,7 @@ export MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ant -buildfile $MYDIR/testing/dataflowexample/build.xml compile-libs
 ln -s $MYDIR/src/dataflow $MYDIR/../checker-framework-inference/src
-mv $MYDIR/../checker-framework-inference/src/dataflow/tests/DataflowTest.java $MYDIR/../checker-framework-inference/tests/checkers/inference
+cp $MYDIR/testing/tests/DataflowTest.java $MYDIR/../checker-framework-inference/tests/checkers/inference
 ln -s $MYDIR/../checker-framework-inference/src/dataflow/tests/testdata/dataflow $MYDIR/../checker-framework-inference/testdata
 ln -s $MYDIR/testing/dataflowexample $MYDIR/..
 gradle -b $MYDIR/../checker-framework-inference/build.gradle dist
