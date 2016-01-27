4060ce08f739cea1257d1101657a51db0ed720bb
Jianchu <jianchuli0624@gmail.com>
2016-01-27 15:42:56 -0500

    add lingelingsat solver

---
+++ src/generalconstraintsolver/dataflowsolver/dataflowsatsolver/DatatypeLingelingSatSolver.java
@@ -0,0 +1,193 @@
+package generalconstraintsolver.dataflowsolver.dataflowsatsolver;
+
+import generalconstraintsolver.dataflowsolver.DataflowImpliesLogic;
+import generalconstraintsolver.satsubsolver.SatSubSolver;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.lang.model.element.AnnotationMirror;
+
+import org.sat4j.core.VecInt;
+
+import checkers.inference.SlotManager;
+import dataflow.util.DataflowUtils;
+
+public class DatatypeLingelingSatSolver extends SatSubSolver {
+    private DataflowImpliesLogic logic;
+    private StringBuilder sb = new StringBuilder();
+    private String input, output, lingeling;
+    private Set<Integer> variableSet = new HashSet<Integer>();
+    List<Integer> resultList = new ArrayList<Integer>();
+    public DatatypeLingelingSatSolver(DataflowImpliesLogic logic,
+            SlotManager slotManager) {
+        super(logic.getLogics(), slotManager, logic.getLattice());
+        this.logic = logic;
+    }
+
+    public int[] dataflowsatSolve() {
+        final String currentPath = new File("").getAbsolutePath();
+        File file = new File(currentPath);
+        String base = file.getParent().toString();
+        createLingelingDir(base);
+
+        Map<Integer, Boolean> idToExistence = new HashMap<>();
+        Map<Integer, AnnotationMirror> result = new HashMap<>();
+        List<VecInt> clauses = convertImpliesToClauses();
+        becomeWellForm(clauses);
+        buildInputString(clauses);
+        return getSolution();
+    }
+
+    private void buildInputString(List<VecInt> clauses) {
+        String datatype = DataflowUtils.getDataflowValue(logic.getLattice().top)[0];
+        sb.append("c This is the input for ");
+        sb.append(datatype + "\n");
+        final int totalVars = (slotManager.nextId() * lattice.numModifiers);
+        final int totalClauses = clauses.size();
+        sb.append("p cnf ");
+        sb.append(totalVars + " ");
+        sb.append(totalClauses + "\n");
+        try {
+            for (VecInt clause : clauses) {
+                // lastClause = clause;
+                int[] clauseArray = clause.toArray();
+                for (int i = 0; i < clauseArray.length; i++) {
+                    variableSet.add(Math.abs(clauseArray[i]));
+                    sb.append(clauseArray[i]);
+                    sb.append(" ");
+                }
+                sb.append("0\n");
+            }
+            wirteCNFinput(datatype);
+            solveSAT(datatype);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void getSolverOutput(String datatype) throws FileNotFoundException {
+        String readPath = input + "/" + datatype;
+        InputStream in = new FileInputStream(readPath);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+                }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void solveSAT(String datatype) throws FileNotFoundException {
+        String readPath = input + "/" + datatype;
+        String command = lingeling + " " + readPath;
+        try {
+            getOutPut_Error(command);
+        } catch (IOException | InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    private void wirteCNFinput(String datatype) throws FileNotFoundException {
+        String writePath = input + "/" + datatype;
+        File f = new File(writePath);
+        PrintWriter pw = new PrintWriter(f);
+        pw.write(sb.toString());
+        pw.close();
+        sb.setLength(0);
+    }
+
+    private void createLingelingDir(String base) {
+        String path = base
+                + "/src/generalconstraintsolver/dataflowsolver/lingelingdata";
+        input = path + "/input";
+        output = path + "/output";
+        lingeling = new File(base).getParent() + "/lingeling/lingeling";
+        createDir(path);
+        createDir(input);
+        createDir(output);
+    }
+
+    private void createDir(String path) {
+        File dirPath = new File(path);
+        dirPath.mkdir();
+    }
+
+    public int[] getSolution() {
+        int[] result = new int[resultList.size()];
+        for (int i = 0; i < resultList.size(); i++) {
+            result[i] = resultList.get(i);
+        }
+        return result;
+    }
+
+    protected void getOutPut_Error(String command)
+            throws IOException, InterruptedException {
+        resultList = new ArrayList<Integer>();
+        final Process p = Runtime.getRuntime().exec(command);
+        Thread getOutPut = new Thread() {
+            public void run() {
+                String s = "";
+                BufferedReader stdInput = new BufferedReader(
+                        new InputStreamReader(p.getInputStream()));
+                try {
+                    while ((s = stdInput.readLine()) != null) {
+                        if (s.charAt(0) == 'v') {
+                            s = s.substring(1);
+                            for (String retval : s.split(" ")) {
+                                if (!retval.equals("") && !retval.equals(" ")
+                                        && !retval.equals("\n")) {
+                                    if (variableSet.contains(Math.abs(Integer
+                                            .parseInt(retval)))) {
+                                        resultList
+                                                .add(Integer.parseInt(retval));
+                                    }
+
+                                }
+
+                            }
+                        }
+                    }
+                } catch (NumberFormatException | IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        getOutPut.start();
+
+        Thread getError = new Thread() {
+            public void run() {
+                String s = "";
+                String errReply = "";
+                BufferedReader stdError = new BufferedReader(
+                        new InputStreamReader(p.getErrorStream()));
+                try {
+                    while ((s = stdError.readLine()) != null) {
+                        errReply = errReply + s;
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        getError.start();
+        getOutPut.join();
+        getError.join();
+        p.waitFor();
+    }
+}
