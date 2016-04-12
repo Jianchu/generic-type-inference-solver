3f4c041e09960e4db1108b9e0136314338002f6b
Werner Dietl <wdietl@gmail.com>
2016-04-11 20:58:00 -0400

    Also include ontology dirs/files. This scripts should be renamed.

--- dataflowSetup.sh
+++ dataflowSetup.sh
@@ -2,8 +2,18 @@
 export MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ant -buildfile $MYDIR/testing/dataflowexample/build.xml compile-libs
+
 ln -s $MYDIR/src/dataflow $MYDIR/../checker-framework-inference/src
-cp $MYDIR/testing/tests/DataflowTest.java $MYDIR/../checker-framework-inference/tests/checkers/inference
+ln -s $MYDIR/src/ontology $MYDIR/../checker-framework-inference/src
+ln -s $MYDIR/src/generalconstraintsolver $MYDIR/../checker-framework-inference/src
+
+
+ln -s $MYDIR/testing/tests/DataflowTest.java $MYDIR/../checker-framework-inference/tests/checkers/inference
+ln -s $MYDIR/testing/tests/OntologyTest.java $MYDIR/../checker-framework-inference/tests/checkers/inference
+
 ln -s $MYDIR/../checker-framework-inference/src/dataflow/tests/testdata/dataflow $MYDIR/../checker-framework-inference/testdata
+ln -s $MYDIR/../checker-framework-inference/src/dataflow/tests/testdata/ontology $MYDIR/../checker-framework-inference/testdata
+
 ln -s $MYDIR/testing/dataflowexample $MYDIR/..
+
 gradle -b $MYDIR/../checker-framework-inference/build.gradle dist
