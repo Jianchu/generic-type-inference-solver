050614b530ad4ad76d8a5633228f2674f8a21cc6
Jianchu <jianchuli0624@gmail.com>
2016-04-18 12:39:48 -0400

    add array list

--- src/ontology/util/OntologyUtils.java
+++ src/ontology/util/OntologyUtils.java
@@ -10,6 +10,8 @@ public class OntologyUtils {
     public static boolean isSequence(TypeMirror type) {
         if (TypesUtils.isDeclaredOfName(type, "java.util.LinkedList")) {
             return true;
+        } else if (TypesUtils.isDeclaredOfName(type, "java.util.ArrayList")) {
+            return true;
         } else if (type.getKind().equals(TypeKind.ARRAY)) {
             return true;
         }
