60a7e02cc9032dfeb66d894b51860e2e09ab5116
Jianchu <jianchuli0624@gmail.com>
2016-04-18 14:35:53 -0400

    add bin to class path

--- scripts/ontology/roundtripOntologyMaxSat.sh
+++ scripts/ontology/roundtripOntologyMaxSat.sh
@@ -4,4 +4,4 @@ export MYDIR=`dirname $0`/..
 . $MYDIR/setup.sh
 export ROOT=$MYDIR/../..
 distDir=$CHINF"/dist"
-java -classpath "$distDir"/checker.jar:"$distDir"/plume.jar:"$distDir"/checker-framework-inference.jar:$ROOT/universe/bin checkers.inference.InferenceLauncher --solverArgs="backEndType=maxsatbackend.MaxSat" --checker ontology.OntologyChecker --solver constraintsolver.ConstraintSolver --mode ROUNDTRIP -afud $ROOT/mini_corpus/sorting/00_sort/annotated $*
+java -classpath "$distDir"/checker.jar:"$distDir"/plume.jar:"$distDir"/checker-framework-inference.jar:$ROOT/generic-type-inference-solver/bin checkers.inference.InferenceLauncher --solverArgs="backEndType=maxsatbackend.MaxSat" --checker ontology.OntologyChecker --solver constraintsolver.ConstraintSolver --mode ROUNDTRIP -afud $ROOT/mini_corpus/sorting/00_sort/annotated $*
