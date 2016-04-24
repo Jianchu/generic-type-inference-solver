1eb08faa228e3f3ca6f1a908ffbc8484fb3f8f5c
Werner Dietl <wdietl@gmail.com>
2016-04-24 15:55:34 -0400

    Instead of a boolean query use a utility method that creates the correct Ontology AnnotationMirror.

--- src/ontology/util/OntologyUtils.java
+++ src/ontology/util/OntologyUtils.java
@@ -1,21 +1,25 @@
 package ontology.util;
 
+import org.checkerframework.javacutil.AnnotationUtils;
 import org.checkerframework.javacutil.TypesUtils;
 
+import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.type.TypeKind;
 import javax.lang.model.type.TypeMirror;
+import javax.lang.model.util.Elements;
+
+import ontology.qual.Sequence;
 
 public class OntologyUtils {
 
-    public static boolean isSequence(TypeMirror type) {
-        if (TypesUtils.isDeclaredOfName(type, "java.util.LinkedList")) {
-            return true;
-        } else if (TypesUtils.isDeclaredOfName(type, "java.util.ArrayList")) {
-            return true;
-        } else if (type.getKind().equals(TypeKind.ARRAY)) {
-            return true;
+    public static AnnotationMirror determineAnnotation(Elements elements, TypeMirror type) {
+        if (TypesUtils.isDeclaredOfName(type, "java.util.LinkedList") ||
+                TypesUtils.isDeclaredOfName(type, "java.util.ArrayList") ||
+                type.getKind().equals(TypeKind.ARRAY)) {
+            AnnotationMirror SEQ = AnnotationUtils.fromClass(elements, Sequence.class);
+            return SEQ;
         }
-        return false;
+        return null;
     }
 
 }
