0d0390499c95a542176d0e0b013c73bda119bf01
Jianchu <jianchuli0624@gmail.com>
2016-04-27 17:55:56 -0400

    remove confusing script for now

--- scripts/ontology/roundtripOntologyMaxSat.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export MYDIR=`dirname $0`/..
-. $MYDIR/setup.sh
-export ROOT=$MYDIR/../..
-CORPUS_DIR=$ROOT/../corpus
-distDir=$CHINF"/dist"
-java -classpath "$distDir"/checker.jar:"$distDir"/plume.jar:"$distDir"/checker-framework-inference.jar:$ROOT/generic-type-inference-solver/bin checkers.inference.InferenceLauncher --solverArgs="backEndType=maxsatbackend.MaxSat" --checker ontology.OntologyChecker --solver constraintsolver.ConstraintSolver --mode ROUNDTRIP -afud $CORPUS_DIR/sorting/00_sort/annotated $*
