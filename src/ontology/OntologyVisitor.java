96d7b534aa3c460703633ab844e8c161e82224c2
Jianchu <jianchuli0624@gmail.com>
2016-04-29 16:02:55 -0400

    use addPreference

--- src/ontology/OntologyVisitor.java
+++ src/ontology/OntologyVisitor.java
@@ -1,21 +1,16 @@
 package ontology;
 
+import ontology.util.OntologyUtils;
+
 import org.checkerframework.common.basetype.BaseAnnotatedTypeFactory;
 import org.checkerframework.framework.type.AnnotatedTypeMirror;
 
 import javax.lang.model.element.AnnotationMirror;
 
-import com.sun.source.tree.VariableTree;
-
-import checkers.inference.ConstraintManager;
 import checkers.inference.InferenceChecker;
-import checkers.inference.InferenceMain;
 import checkers.inference.InferenceVisitor;
-import checkers.inference.SlotManager;
-import checkers.inference.model.ConstantSlot;
-import checkers.inference.model.PreferenceConstraint;
-import checkers.inference.model.VariableSlot;
-import ontology.util.OntologyUtils;
+
+import com.sun.source.tree.VariableTree;
 
 public class OntologyVisitor extends InferenceVisitor<OntologyChecker, BaseAnnotatedTypeFactory> {
 
@@ -26,15 +21,11 @@ public class OntologyVisitor extends InferenceVisitor<OntologyChecker, BaseAnnot
 
     @Override
     public Void visitVariable(VariableTree node, Void p) {
+        int weight = 50;
         AnnotatedTypeMirror type = atypeFactory.getAnnotatedType(node);
-        ConstraintManager cManager = InferenceMain.getInstance().getConstraintManager();
-        SlotManager sManager = InferenceMain.getInstance().getSlotManager();
-        VariableSlot vSlot = sManager.getVariableSlot(type);
         AnnotationMirror anno = OntologyUtils.determineAnnotation(elements, type.getUnderlyingType());
         if (anno != null) {
-            // JLTODO: Is it good to use 'new ConstantSlot' here instead of the
-            // 'variableAnnotator.createConstant' used in the ATF?
-            cManager.add(new PreferenceConstraint(vSlot, new ConstantSlot(anno, sManager.nextId()), 50));
+            this.addPreference(type, anno, weight);
         }
         return super.visitVariable(node, p);
     }
