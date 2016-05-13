6111f2ae00f3fd642e06ad8bc9651f200d5aab9a
Jianchu <jianchuli0624@gmail.com>
2016-05-13 10:10:11 -0400

    add equality constrant

--- src/logiqlbackend/LogiQLSerializer.java
+++ src/logiqlbackend/LogiQLSerializer.java
@@ -1,9 +1,11 @@
 package logiqlbackend;
 
+import util.NameUtils;
 import checkers.inference.model.CombVariableSlot;
 import checkers.inference.model.CombineConstraint;
 import checkers.inference.model.ComparableConstraint;
 import checkers.inference.model.ConstantSlot;
+import checkers.inference.model.Constraint;
 import checkers.inference.model.EqualityConstraint;
 import checkers.inference.model.ExistentialConstraint;
 import checkers.inference.model.ExistentialVariableSlot;
@@ -11,6 +13,7 @@ import checkers.inference.model.InequalityConstraint;
 import checkers.inference.model.PreferenceConstraint;
 import checkers.inference.model.RefinementVariableSlot;
 import checkers.inference.model.Serializer;
+import checkers.inference.model.Slot;
 import checkers.inference.model.SubtypeConstraint;
 import checkers.inference.model.VariableSlot;
 
@@ -24,8 +27,32 @@ public class LogiQLSerializer implements Serializer<String, String> {
 
     @Override
     public String serialize(EqualityConstraint constraint) {
-        // TODO Auto-generated method stub
-        return null;
+        return new VariableCombos<EqualityConstraint>() {
+
+            @Override
+            protected String constant_variable(ConstantSlot slot1, VariableSlot slot2, EqualityConstraint constraint) {
+                String constantName = NameUtils.getSimpleName(slot1.getValue());
+                int variableId = slot2.getId();
+                String encoding = "+equalityConstraintContainsConstant(c, v), +constant(c), +hasconstantName[c] = \""
+                        + constantName + "\", +variable(v), +hasvariableName[v] = " + variableId + ".\n";
+                return encoding;
+            }
+
+            @Override
+            protected String variable_constant(VariableSlot slot1, ConstantSlot slot2,
+                    EqualityConstraint constraint) {
+                return constant_variable(slot2, slot1, constraint);
+            }
+
+            @Override
+            protected String variable_variable(VariableSlot slot1, VariableSlot slot2,
+                    EqualityConstraint constraint) {
+                String encoding = "+equalityConstraint(v1, v2), +variable(v1), +hasvariableName[v1] = "
+                        + slot1.getId() + ", +variable(v2), +hasvariableName[v2] = " + slot2.getId()
+                        + ".";
+                return encoding;
+            }
+        }.accept(constraint.getFirst(), constraint.getSecond(), constraint);
     }
 
     @Override
@@ -87,5 +114,48 @@ public class LogiQLSerializer implements Serializer<String, String> {
         // TODO Auto-generated method stub
         return null;
     }
+
+    class VariableCombos<T extends Constraint> {
+
+        protected String variable_variable(VariableSlot slot1, VariableSlot slot2, T constraint) {
+            return defaultAction(slot1, slot2, constraint);
+        }
+
+        protected String constant_variable(ConstantSlot slot1, VariableSlot slot2, T constraint) {
+            return defaultAction(slot1, slot2, constraint);
+        }
+
+        protected String variable_constant(VariableSlot slot1, ConstantSlot slot2, T constraint) {
+            return defaultAction(slot1, slot2, constraint);
+        }
+
+        protected String constant_constant(ConstantSlot slot1, ConstantSlot slot2, T constraint) {
+            return defaultAction(slot1, slot2, constraint);
+        }
+
+        public String defaultAction(Slot slot1, Slot slot2, T constraint) {
+            return emptyString;
+        }
+
+        public String accept(Slot slot1, Slot slot2, T constraint) {
+
+            final String result;
+
+            if (slot1 instanceof ConstantSlot) {
+                if (slot2 instanceof ConstantSlot) {
+                    result = constant_constant((ConstantSlot) slot1, (ConstantSlot) slot2, constraint);
+                } else {
+                    result = constant_variable((ConstantSlot) slot1, (VariableSlot) slot2, constraint);
+                }
+            } else if (slot2 instanceof ConstantSlot) {
+                result = variable_constant((VariableSlot) slot1, (ConstantSlot) slot2, constraint);
+            } else {
+                result = variable_variable((VariableSlot) slot1, (VariableSlot) slot2, constraint);
+            }
+            return result;
+        }
+    }
+
+    public static final String emptyString = "";
 
 }
