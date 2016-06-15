adf345104dbd08a5ec2c7a91b7ed4bcf72e52cb3
Jianchu <jianchuli0624@gmail.com>
2016-06-15 15:25:17 -0400

    use InferenceATF changes in CFI

--- src/dataflow/DataflowAnnotatedTypeFactory.java
+++ src/dataflow/DataflowAnnotatedTypeFactory.java
@@ -4,7 +4,6 @@ import org.checkerframework.common.basetype.BaseAnnotatedTypeFactory;
 import org.checkerframework.common.basetype.BaseTypeChecker;
 import org.checkerframework.framework.type.AnnotatedTypeMirror;
 import org.checkerframework.framework.type.QualifierHierarchy;
-import org.checkerframework.framework.type.treeannotator.ImplicitsTreeAnnotator;
 import org.checkerframework.framework.type.treeannotator.ListTreeAnnotator;
 import org.checkerframework.framework.type.treeannotator.TreeAnnotator;
 import org.checkerframework.framework.util.GraphQualifierHierarchy;
@@ -25,12 +24,6 @@ import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.type.TypeMirror;
 
-import checkers.inference.InferenceAnnotatedTypeFactory;
-import checkers.inference.InferrableAnnotatedTypeFactory;
-import checkers.inference.InferrableChecker;
-import checkers.inference.SlotManager;
-import checkers.inference.VariableAnnotator;
-
 import com.sun.source.tree.LiteralTree;
 import com.sun.source.tree.MethodInvocationTree;
 import com.sun.source.tree.NewClassTree;
@@ -39,8 +32,7 @@ import dataflow.qual.DataFlow;
 import dataflow.qual.DataFlowTop;
 import dataflow.util.DataflowUtils;
 
-public class DataflowAnnotatedTypeFactory extends BaseAnnotatedTypeFactory
-        implements InferrableAnnotatedTypeFactory {
+public class DataflowAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
 
     protected final AnnotationMirror DATAFLOW, DATAFLOWBOTTOM, DATAFLOWTOP;
     private ExecutableElement dataflowValue = TreeUtils.getMethod(
@@ -63,16 +55,6 @@ public class DataflowAnnotatedTypeFactory extends BaseAnnotatedTypeFactory
                 super.createTreeAnnotator(),
                 new DataflowTreeAnnotator()
         );
-    }
-
-    @Override
-    public TreeAnnotator getInferenceTreeAnnotator(
-            InferenceAnnotatedTypeFactory atypeFactory,
-            InferrableChecker realChecker,
-            VariableAnnotator variableAnnotator, SlotManager slotManager) {
-        return new ListTreeAnnotator(new ImplicitsTreeAnnotator(this),
-                new DataflowInferenceTreeAnnotator(atypeFactory, realChecker,
-                        this, variableAnnotator, slotManager));
     }
 
     @Override
---
+++ src/dataflow/DataflowInferenceAnnotatedTypeFactory.java
@@ -0,0 +1,92 @@
+package dataflow;
+
+import org.checkerframework.common.basetype.BaseAnnotatedTypeFactory;
+import org.checkerframework.framework.type.AnnotatedTypeMirror;
+import org.checkerframework.framework.type.AnnotatedTypeMirror.AnnotatedDeclaredType;
+import org.checkerframework.framework.type.AnnotatedTypeMirror.AnnotatedPrimitiveType;
+import org.checkerframework.framework.type.treeannotator.ImplicitsTreeAnnotator;
+import org.checkerframework.framework.type.treeannotator.ListTreeAnnotator;
+import org.checkerframework.framework.type.treeannotator.TreeAnnotator;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.PrimitiveType;
+
+import checkers.inference.ConstraintManager;
+import checkers.inference.InferenceAnnotatedTypeFactory;
+import checkers.inference.InferenceChecker;
+import checkers.inference.InferenceMain;
+import checkers.inference.InferrableChecker;
+import checkers.inference.SlotManager;
+import checkers.inference.model.ConstantSlot;
+import dataflow.util.DataflowUtils;
+
+public class DataflowInferenceAnnotatedTypeFactory extends InferenceAnnotatedTypeFactory {
+
+    public DataflowInferenceAnnotatedTypeFactory(InferenceChecker inferenceChecker,
+            boolean withCombineConstraints, BaseAnnotatedTypeFactory realTypeFactory,
+            InferrableChecker realChecker, SlotManager slotManager, ConstraintManager constraintManager) {
+        super(inferenceChecker, withCombineConstraints, realTypeFactory, realChecker, slotManager,
+                constraintManager);
+    }
+
+    @Override
+    public TreeAnnotator createTreeAnnotator() {
+        return new ListTreeAnnotator(new ImplicitsTreeAnnotator(this),
+                new DataflowInferenceTreeAnnotator(this, realChecker, realTypeFactory, variableAnnotator,
+                        slotManager));
+    }
+
+    @Override
+    public AnnotatedDeclaredType getBoxedType(AnnotatedPrimitiveType type) {
+        TypeElement typeElt = types.boxedClass(type.getUnderlyingType());
+        AnnotationMirror am = createDataflow(typeElt.asType().toString(), this.processingEnv);
+        AnnotatedDeclaredType dt = fromElement(typeElt);
+        // dt.replaceAnnotation(am);
+        // System.out.println("type:" + type.getAnnotations());
+        // System.out.println("dt:" + dt.getAnnotations());
+
+        // dt.addAnnotations(type.getAnnotations());
+        ConstantSlot cs = new ConstantSlot(am, InferenceMain.getInstance().getSlotManager().nextId());
+        InferenceMain.getInstance().getSlotManager().addVariable(cs);
+        dt.addAnnotation(InferenceMain.getInstance().getSlotManager().getAnnotation(cs));
+        dt.addAnnotation(cs.getValue());
+        // AnnotationBuilder ab = new AnnotationBuilder(this.processingEnv,
+        // VarAnnot.class);
+        // ab.setValue("value",
+        // InferenceMain.getInstance().getSlotManager().nextId());
+        // dt.addAnnotation(ab.build());
+        // dt.addAnnotation(am);
+        // dt.addAnnotation(cs.getValue());
+        // dt.addAnnotation(am);
+        return dt;
+    }
+
+    @Override
+    public AnnotatedPrimitiveType getUnboxedType(AnnotatedDeclaredType type)
+            throws IllegalArgumentException {
+        PrimitiveType primitiveType = types.unboxedType(type.getUnderlyingType());
+        AnnotationMirror am = createDataflow(primitiveType.toString(), this.processingEnv);
+        AnnotatedPrimitiveType pt = (AnnotatedPrimitiveType) AnnotatedTypeMirror.createType(
+                primitiveType, this, false);
+        ConstantSlot cs = new ConstantSlot(am, InferenceMain.getInstance().getSlotManager().nextId());
+        InferenceMain.getInstance().getSlotManager().addVariable(cs);
+        pt.addAnnotation(InferenceMain.getInstance().getSlotManager().getAnnotation(cs));
+        pt.addAnnotation(cs.getValue());
+        // pt.addAnnotation(am);
+        // pt.addAnnotations(type.getAnnotations());
+        return pt;
+    }
+
+    private AnnotationMirror createDataflow(String typeName, ProcessingEnvironment processingEnv) {
+        Set<String> typeNames = new HashSet<String>();
+        typeNames.add(typeName);
+        AnnotationMirror am = DataflowUtils.createDataflowAnnotation(typeNames, processingEnv);
+        return am;
+    }
+
+}
