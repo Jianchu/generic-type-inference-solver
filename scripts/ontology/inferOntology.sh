148982fdb463a79b3d7574d62f20c35ff696c388
Jianchu <jianchuli0624@gmail.com>
2016-06-16 10:56:33 -0400

    remove outdated sh

---
+++ scripts/ontology/inferOntology.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export MYDIR=`dirname $0`/..
+. $MYDIR/setup.sh
+export ROOT=$MYDIR/../..
+distDir=$CHINF"/dist"
+java -classpath "$distDir"/checker.jar:"$distDir"/plume.jar:"$distDir"/checker-framework-inference.jar:$ROOT/generic-type-inference-solver/bin  checkers.inference.InferenceLauncher --checker ontology.OntologyChecker --solver ontology.solver.OntologySolver --mode INFER $*
