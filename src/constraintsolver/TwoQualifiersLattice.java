27df1f449228cdb8e62b63ca68ed8c053a0c7000
Jianchu <jianchuli0624@gmail.com>
2016-06-21 16:06:54 -0400

    two qualifier lattice

--- src/constraintsolver/TwoQualifiersLattice.java
+++ src/constraintsolver/TwoQualifiersLattice.java
@@ -2,9 +2,18 @@ package constraintsolver;
 
 import org.checkerframework.framework.type.QualifierHierarchy;
 
+import javax.lang.model.element.AnnotationMirror;
+
 public class TwoQualifiersLattice extends Lattice {
 
     public TwoQualifiersLattice(QualifierHierarchy qualHierarchy) {
         super(qualHierarchy);
+    }
+
+    public TwoQualifiersLattice(QualifierHierarchy qualHierarchy, AnnotationMirror top,
+            AnnotationMirror bottom) {
+        super(qualHierarchy);
+        this.top = top;
+        this.bottom = bottom;
     }
 }
