d34a37c73d75a9f9e26702e0fe701a638cef97b4
Jianchu <jianchuli0624@gmail.com>
2016-06-28 17:49:40 -0400

    update example

--- testing/dataflowexample/libs/src/Foo.java
+++ testing/dataflowexample/libs/src/Foo.java
@@ -1,13 +1,25 @@
 package libs;
 
+import java.util.*;
+
 public class Foo {
-	private int fieldOfFoo;
+    
+    private int fieldOfFoo;
 
-	public Foo(int fieldOfFoo){
-		this.fieldOfFoo = fieldOfFoo;
-	}
+    public Foo(int fieldOfFoo){
+        this.fieldOfFoo = fieldOfFoo;
+    }
 
-	public int getField(){
-		return fieldOfFoo;
-	}
+    public int getField(){
+        return fieldOfFoo;
+    }
+
+    public static List<String> foo1() {
+        return null;
+    }
+
+    public static Number[] foo2() {
+        return null;
+    }
+
 }
--- testing/dataflowexample/project/src/Bar.java
+++ testing/dataflowexample/project/src/Bar.java
@@ -1,33 +1,71 @@
 package project;
 
 import libs.Foo;
-import java.util.Date;
+import java.util.*;
 
 public class Bar {
 
-	private Foo fieldOfBar;
-	int thisIsInt = 3;
+    Foo fieldOfBar;
 
     char thisIsChar = 'c';
 
     String thisIsString = "String!";
+    
     Object thisShouldbeString = thisIsString;
 
     Date date = new Date();
 
-	public Bar(){
-		this.fieldOfBar = new Foo(10);
-	}
+    Integer autoboxing = 1;
 
-    public Object testingUpperBound(int intpara) {
+    int unboxing = new Integer(1);
 
-        if (intpara == 1) {
+    int noAutoboxing = 1;
+
+    Integer noUnboxing = new Integer(1);
+
+    Bar(){
+        this.fieldOfBar = new Foo(10);
+    }
+
+    Object testingUpperBound1() {
+
+        if (autoboxing == 1) {
             return 3;
-        } else if (intpara == 2) {
+        } else if (unboxing == 2) {
             return 3.14;
-        } else if (intpara == 3) {
+        } else if (noAutoboxing == 3) {
             return 3.1415926;
         }
         return "I am a String!";
+    }
+    
+    Object testingUpperBound2() {
+        if(autoboxing > 0) {
+            return new LinkedList<String>();
+        } else if (unboxing < 0) {
+            return new ArrayList<String>();
+        } else if (noAutoboxing > 0) {
+            return Foo.foo1(); 
+        } else if (noUnboxing > 0) {
+            return new Object().toString();
+        } else {
+            return new Object();
+        }
+    }
+
+    Object o = new Object();
+
+    void testingUpperBound3() {
+        if(autoboxing > 0) {
+            o = new Integer[1];
+        } else if (unboxing < 0) {
+            o = new Double[1];
+        } else if (noAutoboxing > 0) {
+            o = Foo.foo2(); 
+        } else if (noUnboxing > 0) {
+            o = new String[1];
+        } else {
+            o = new Object().toString();
+        }
     }
 }
