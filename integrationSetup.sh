f9d7f88ccc589e310c455776f9d7c54954352d14
Jianchu <jianchuli0624@gmail.com>
2016-06-28 12:45:07 -0400

    remove ontology

--- integrationSetup.sh
+++ integrationSetup.sh
@@ -10,7 +10,7 @@ export MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ant -buildfile $MYDIR/testing/dataflowexample/build.xml compile-libs
 
 ln -s $MYDIR/src/dataflow $MYDIR/../checker-framework-inference/src
-ln -s $MYDIR/src/ontology $MYDIR/../checker-framework-inference/src
+#ln -s $MYDIR/src/ontology $MYDIR/../checker-framework-inference/src
 ln -s $MYDIR/src/constraintsolver $MYDIR/../checker-framework-inference/src
 ln -s $MYDIR/src/generalconstraintsolver $MYDIR/../checker-framework-inference/src
 ln -s $MYDIR/src/maxsatbackend $MYDIR/../checker-framework-inference/src
@@ -20,10 +20,10 @@ ln -s $MYDIR/src/logiqlbackend $MYDIR/../checker-framework-inference/src
 ln -s $MYDIR/src/constraintgraph $MYDIR/../checker-framework-inference/src
 
 ln -s $MYDIR/testing/tests/DataflowTest.java $MYDIR/../checker-framework-inference/tests/checkers/inference
-ln -s $MYDIR/testing/tests/OntologyTest.java $MYDIR/../checker-framework-inference/tests/checkers/inference
+#ln -s $MYDIR/testing/tests/OntologyTest.java $MYDIR/../checker-framework-inference/tests/checkers/inference
 
 ln -s $MYDIR/testing/dataflow $MYDIR/../checker-framework-inference/testdata
-ln -s $MYDIR/testing/ontology $MYDIR/../checker-framework-inference/testdata
+#ln -s $MYDIR/testing/ontology $MYDIR/../checker-framework-inference/testdata
 
 ln -s $MYDIR/testing/dataflowexample $MYDIR/..
 
