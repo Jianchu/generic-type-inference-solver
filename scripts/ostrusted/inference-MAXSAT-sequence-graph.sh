730d688f427596afbc00311671597017a352ef79
Jianchu <jianchuli0624@gmail.com>
2016-07-05 15:35:16 -0400

    add some scripts

---
+++ scripts/ostrusted/inference-MAXSAT-sequence-graph.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export MYDIR=`dirname $0`/..
+. $MYDIR/setup.sh
+export ROOT=$MYDIR/../..
+distDir=$CHINF"/dist"
+java -classpath "$distDir"/checker.jar:"$distDir"/plume.jar:"$distDir"/checker-framework-inference.jar:$ROOT/generic-type-inference-solver/bin checkers.inference.InferenceLauncher --solverArgs="backEndType=maxsatbackend.MaxSat,solveInParallel=false" --checker ostrusted.OsTrustedChecker --solver constraintsolver.ConstraintSolver --mode INFER --hacks=true $*
