c9137442c74c762bb880500fdf87b0b850eb6030
Jianchu <jianchuli0624@gmail.com>
2016-07-06 17:00:10 -0400

    script add current location

--- scripts/dataflow/inference-MAXSAT-parallel.sh
+++ scripts/dataflow/inference-MAXSAT-parallel.sh
@@ -4,4 +4,4 @@ export MYDIR=`dirname $0`/..
 . $MYDIR/setup.sh
 export ROOT=$MYDIR/../..
 distDir=$CHINF"/dist"
-java -classpath "$distDir"/checker.jar:"$distDir"/plume.jar:"$distDir"/checker-framework-inference.jar:$ROOT/generic-type-inference-solver/bin checkers.inference.InferenceLauncher --solverArgs="backEndType=maxsatbackend.MaxSat" --checker dataflow.DataflowChecker --solver dataflow.solvers.backend.DataflowConstraintSolver --mode INFER --hacks=true $*
+java -classpath "$distDir"/checker.jar:"$distDir"/plume.jar:"$distDir"/checker-framework-inference.jar:$ROOT/generic-type-inference-solver/bin:. checkers.inference.InferenceLauncher --solverArgs="backEndType=maxsatbackend.MaxSat" --checker dataflow.DataflowChecker --solver dataflow.solvers.backend.DataflowConstraintSolver --mode INFER --hacks=true $*
