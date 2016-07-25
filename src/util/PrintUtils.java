a8fd447e359b02051d14e96b6dcaa56a43dccbf7
Jianchu <jianchuli0624@gmail.com>
2016-07-25 13:27:48 -0400

    use stringbuilder for print

--- src/util/PrintUtils.java
+++ src/util/PrintUtils.java
@@ -1,5 +1,8 @@
 package util;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Map;
 
 import javax.lang.model.element.AnnotationMirror;
@@ -16,17 +19,50 @@ public class PrintUtils {
     public static void printResult(Map<Integer, AnnotationMirror> result) {
 
         final int maxLength = String.valueOf(InferenceMain.getInstance().getSlotManager().nextId()).length();
-
+        StringBuilder printResult = new StringBuilder();
         System.out.println("/***********************results****************************/");
         for (Integer j : result.keySet()) {
-            String resultStr = "SlotID: " + j;
+            printResult.append("SlotID: ");
+            printResult.append(String.valueOf(j));
             for (int i = 0; i < maxLength + 2 - String.valueOf(j).length(); i++) {
-                resultStr = resultStr + " ";
+                printResult.append(" ");
             }
-            resultStr = resultStr + "Annotation: " + result.get(j).toString();
-            System.out.println(resultStr);
+            printResult.append("Annotation: ");
+            printResult.append(result.get(j).toString());
+            printResult.append("\n");
         }
+        System.out.println(printResult.toString());
         System.out.flush();
         System.out.println("/**********************************************************/");
+    }
+
+    public static void writeResult(Map<Integer, AnnotationMirror> result) {
+        StringBuilder printResult = new StringBuilder();
+        final int maxLength = String.valueOf(InferenceMain.getInstance().getSlotManager().nextId())
+                .length();
+
+        for (Integer j : result.keySet()) {
+            printResult.append("SlotID: ");
+            printResult.append(String.valueOf(j));
+            for (int i = 0; i < maxLength + 2 - String.valueOf(j).length(); i++) {
+                printResult.append(" ");
+            }
+            printResult.append("Annotation: ");
+            printResult.append(result.get(j).toString());
+            printResult.append("\n");
+        }
+
+        File CNFData = new File(new File("").getAbsolutePath());
+        String writePath = CNFData.getAbsolutePath() + "/result" + ".txt";
+        File f = new File(writePath);
+        PrintWriter pw;
+        try {
+            pw = new PrintWriter(f);
+            pw.write(printResult.toString());
+            pw.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        printResult = null;
     }
 }
