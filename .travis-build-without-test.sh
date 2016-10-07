97122a11067520e11331f7e6f7808ae99b735001
CharlesZ-Chen <charleszhuochen@gmail.com>
2016-10-07 19:35:52 -0400

    use TRAVIS_SLUG to download correct CF Inference repo

--- .travis-build-without-test.sh
+++ .travis-build-without-test.sh
@@ -14,12 +14,22 @@ export CHECKERFRAMEWORK=$ROOT/checker-framework
 
 export PATH=$AFU/scripts:$JAVA_HOME/bin:$PATH
 
+#TODO: this split logic is copy from checker-framework-inference's travis script
+#TODO: should refactor later on
+# Split $TRAVIS_REPO_SLUG into the owner and repository parts
+OIFS=$IFS
+IFS='/'
+read -r -a slugarray <<< "$TRAVIS_REPO_SLUG"
+SLUGOWNER=${slugarray[0]}
+SLUGREPO=${slugarray[1]}
+IFS=$OIFS
+
 ## Build Checker Framework
 if [ -d $ROOT/checker-framework-inference ] ; then
     # Older versions of git don't support the -C command-line option
     (cd $ROOT/checker-framework-inference && git pull)
 else
-    (cd $ROOT && git clone https://github.com/pascaliUWat/checker-framework-inference.git)
+    (cd $ROOT && git clone https://github.com/"$SLUGOWNER"/checker-framework-inference.git)
 fi
 
 # This also builds annotation-tools and jsr308-langtools
