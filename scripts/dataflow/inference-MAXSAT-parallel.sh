97bcaea63c7e10583fcfb430feb71c5cef2285a5
Jianchu <jianchuli0624@gmail.com>
2017-01-26 11:26:45 -0500

    update scripts

--- scripts/dataflow/inference-MAXSAT-parallel.sh
+++ scripts/dataflow/inference-MAXSAT-parallel.sh
@@ -5,6 +5,6 @@ export MYDIR=`dirname $0`/..
 export ROOT=$MYDIR/../..
 distDir=$CHINF"/dist"
 
-export CLASSPATH=$ROOT/generic-type-inference-solver/bin:.
+export CLASSPATH=$ROOT/generic-type-inference-solver/bin:/home/jianchu/jsr308/checker-framework-inference/dist/plume.jar:.
 
 $ROOT/checker-framework-inference/scripts/inference-dev checkers.inference.InferenceLauncher --solverArgs="backEndType=maxsatbackend.MaxSat" --checker dataflow.DataflowChecker --solver dataflow.solvers.backend.DataflowConstraintSolver --mode INFER --hacks=true $*
