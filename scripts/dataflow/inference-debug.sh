b5b4cf4d86d2a2d7cb9ebfb4befaabe88e0bcf5f
Jianchu <jianchuli0624@gmail.com>
2017-01-27 14:49:29 -0500

    update script

--- scripts/dataflow/inference-debug.sh
+++ scripts/dataflow/inference-debug.sh
@@ -5,6 +5,6 @@ export MYDIR=`dirname $0`/..
 export ROOT=$MYDIR/../..
 distDir=$CHINF"/dist"
 
-export CLASSPATH=$ROOT/generic-type-inference-solver/bin:.
+export CLASSPATH=$ROOT/generic-type-inference-solver/bin:/home/jianchu/jsr308/checker-framework-inference/dist/plume.jar:.
 
 $ROOT/checker-framework-inference/scripts/inference-dev checkers.inference.InferenceLauncher --checker dataflow.DataflowChecker --solver checkers.inference.solver.DebugSolver --mode INFER --hacks=true $*
